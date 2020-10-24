17ffeda460e1792e43b27c2630320e912d4aac65
Ulrik Egede <u.egede@imperial.ac.uk>
2020-10-24 22:30:44 +1100

    Add the manager of the widgets

--- src/framework/utils.py
+++ src/framework/utils.py
@@ -1,4 +1,6 @@
 import matplotlib
+import time
+import asyncio
 
 
 # https://stackoverflow.com/a/37999370
@@ -13,6 +15,17 @@ def move_figure(f, x, y):
         # This works for QT and GTK
         # You can also use window.setGeometry
         f.canvas.manager.window.move(x, y)
+
+
+async def worker(widget):
+    sleep = widget.updateinterval
+    start = time.monotonic()
+    while True:
+        widget.display('X')
+        passed = time.monotonic() - start
+        sleeptime = -passed % sleep
+        await asyncio.sleep(sleeptime)
+
 
 
 if __name__ == '__main__':
